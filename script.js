46333ead817c3c665aae44b3b5a64151ed02810f
anacarolinacerqueira <cf.anacarolina@gmail.com>
2024-08-05 01:35:26 -0300

    cria regras de senha

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-function validarForm() {
-    var nome = document.getElementById("nome-contato").value;
-    var email = document.getElementById("email-contato").value;
-    var mensagem = document.getElementById("mensagem-contato").value;
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form[data-cy="form-login"]');
+    
+    form.addEventListener('submit', function(event) {
+        validarLogin();
+    });
+});
 
-    if (nome === "" || email === "" || mensagem === "") {
-        alert("Todos os campos marcados com '*' são obrigatórios. Por favor, preencha todos os campos.");
-        return false; // Impede o envio do formulário
+function validarLogin() {
+    const loginInput = document.getElementById('login').value.trim();
+    const senhaInput = document.getElementById('senha').value.trim();
+    const form = document.querySelector('form[data-cy="form-login"]');
+    const mensagemErro = document.createElement('p');
+    mensagemErro.style.color = 'red';
+    mensagemErro.style.fontSize = '12px'
+
+    if (loginInput === '' && senhaInput === '') {
+        mensagemErro.textContent = 'É preciso informar o login e a senha';
+    } else if (loginInput === '') {
+        mensagemErro.textContent = 'É preciso informar o login';
+    } else if (senhaInput === '') {
+        mensagemErro.textContent = 'É preciso informar a senha';
+    } else if (loginInput !== 'teste' || senhaInput !== 'tcc') {
+        mensagemErro.textContent = 'Login ou senha incorretos. Verifique suas credenciais :)';
     } else {
-        alert("Sua mensagem foi enviada. Retornaremos em breve!");
+        window.Location.href = 'index.html';
+        return;
     }
 
-    // Se todos os campos estiverem preenchidos, o formulário será enviado
-    return true;
+    
+    const existingError = form.querySelector('p');
+    if (existingError) {
+        form.removeChild(existingError);
+    }
+    form.appendChild(mensagemErro);
 }
