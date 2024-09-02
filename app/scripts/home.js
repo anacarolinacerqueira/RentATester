6163e694fb8726e5f68b48d52bbdf4ce85982660
anacarolinacerqueira <cf.anacarolina@gmail.com>
2024-09-02 12:01:44 -0300

    ajusta script para exibir usuário fixo se o usuário logado for o default

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -8,6 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
             <strong>Seja bem vindo(a), ${currentUser.nome}!</strong><br> Agora é possível conhecer nossos serviços e fazer um orçamento.
         `;
     }
+    else{
+        const alerta = document.querySelector('.alerta');
+        alerta.innerHTML = `
+            <span class="fechar-alerta" onclick="this.parentElement.style.display='none';">&times;</span>
+            <strong>Seja bem vindo(a), Tester!</strong><br> Agora é possível conhecer nossos serviços e fazer um orçamento.
+        `;
+    }
 });
 
 function rotaOrcamento() {
